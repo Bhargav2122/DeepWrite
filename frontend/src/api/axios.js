54958678b7fb856d35907d7b1d52dc6a448a73cf
Bhargav2122 <balabhargavsurepalli@gmail.com>
2025-12-21 23:19:35 +0530

    change cors file

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 const api = axios.create({
-   // baseURL: `${import.meta.env.VITE_API_URL}/api`,
-   baseURL:'http://localhost:2000/api',
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
+   //baseURL:'http://localhost:2000/api',
     withCredentials: true
 })
 
